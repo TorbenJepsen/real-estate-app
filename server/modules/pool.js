f31d76b433480b12e50dda66a2a2836ac37330db
Torben Jepsen <torbenjepsen@outlook.com>
2018-05-08 11:39:43 -0500

    more heroku stuff

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,27 @@
 const pg = require('pg');
 const Pool = pg.Pool;
+const url = require('url');
 
 // process.env.DATABASE_URL
 
 let config = {};
 
-if (process.env.DATABASE_URL) {
+//postgres://ecjwdhrzqcnjoo:f0ccae602d9ffbffd42f8a1180676451af46270ee290141e6104985e3deda6b0@ec2-50-19-224-165.compute-1.amazonaws.com:5432/dd90va5d47isqe
 
+if (process.env.DATABASE_URL) {
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true, // heroku requires ssl to be true
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
 } else {
     //not on heroku, no process.env.DATABASE_URL
     config = {
