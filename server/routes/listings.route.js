ce575c57afb59ee4288e677094e1cd74c236ee3f
Torben Jepsen <torbenjepsen@outlook.com>
2018-05-08 11:20:58 -0500

    moved pool to module

--- server/routes/listings.route.js
+++ server/routes/listings.route.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-const Pool = pg.Pool;
 
-const pool = new Pool({
-    database: 'real-estate',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
+const pool = require('../modules/pool');
 
-pool.on('connect', () => {
-    console.log('Postresql connected');
-});
 
-pool.on('error', (error) => {
-    console.log('Error with postgres pool', error);
-});
 
 router.get('/', (req, res) => {
     pool.query(`SELECT * FROM "listings" WHERE "type" = 'sale';`)
--- server/routes/rental.route.js
+++ server/routes/rental.route.js
@@ -1,23 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-const Pool = pg.Pool;
-
-const pool = new Pool({
-    database: 'real-estate',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
-
-pool.on('connect', () => {
-    console.log('Postresql connected');
-});
-
-pool.on('error', (error) => {
-    console.log('Error with postgres pool', error);
-});
+const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
     pool.query(`SELECT * FROM "listings" WHERE "type" = 'rent';`)
